5bd129ecd23ba4bbfca54fadef51ee435c9d02e7
trk0lu <brucedmayenschein@gmail.com>
2020-10-22 11:44:34 -0700

    Place holder Calculator CSS and HTML, and working calculator using global variables

--- script.js
+++ script.js
@@ -15,19 +15,72 @@ function divide(a,b) {
 }
 
 function operate(operator, a, b) {
+    let numA = Number(a);
+    let numB = Number(b);
     if (operator == "+") {
-        return add(a, b);
+        return add(numA, numB);
     }
     else if (operator == "-") {
-        return subtract(a,b);
+        return subtract(numA, numB);
     }
     else if (operator == "*") {
-        return multiply(a,b);
+        return multiply(numA, numB);
     }
     else if (operator == "/") {
-        return divide(a,b);
+        return divide(numA, numB);
     }
 }
 
-console.log(operate('/', 10, 5));
-console.log(multiply(10,10))
+let g_Operator = '+';
+let g_a = '0';
+let g_b = '0';
+
+function updateDisplay(e) {
+    display = document.getElementById('display');
+    display.innerText = display.innerText + e.target.id;
+}
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((number) => {
+    number.addEventListener("click", updateDisplay)
+});
+
+function gofuckyourself() {
+    return document.getElementById('display');
+}
+
+function oper(e) {
+    display = document.getElementById('display');
+    g_b = display.innerText;
+    g_a = operate(g_Operator, g_a, g_b);
+    g_Operator = e.target.innerText;
+    display.innerText = " ";
+}
+
+function clear() {
+    display = document.getElementById('display');
+    display.innerText = ' ';
+    g_a = 0;
+    g_b = 0;
+}
+
+const clearscreen = document.getElementById('ac');
+clearscreen.addEventListener('click', clear);
+
+function getEqual(operator, a) {
+    let b = document.getElementById('display').innerText;
+    console.log(operate(g_Operator, g_a, b));
+    ans = operate(g_Operator, g_a, b);
+    display = document.getElementById('display');
+    display.innerText = ans;
+
+}
+
+const EQ = document.getElementById('EQ');
+EQ.addEventListener("click", getEqual);
+
+const op = document.querySelectorAll('.operator');
+op.forEach((op) => {
+    op.addEventListener("click", oper)
+});
+
